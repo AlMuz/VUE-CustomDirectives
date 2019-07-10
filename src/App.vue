db988e272d529350a04383e9d72c1997577cb68e
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-07-10 12:34:47 +0300

    Using Multiple Modifiers

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'blue'">Some text</p>
-                <p v-local-highlight:background="'red'">Some text</p>
+                <p v-local-highlight:background.delayed.blink="'red'">Some text</p>
             </div>
         </div>
     </div>
@@ -26,13 +26,32 @@
               delay = 3000;
             }
 
-            setTimeout(() => {
-              if (binding.arg == 'background') {
-                el.style.backgroundColor = binding.value;
-              } else {
-                el.style.color = binding.value;
-              }
-            }, delay);
+            if (binding.modifiers['blink']) {
+
+              let mainColor = binding.value;
+              let secondColor = 'blue';
+              let currentColor = mainColor;
+
+              setTimeout(() => {
+                setInterval(() => {
+                  currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                  if (binding.arg == 'background') {
+                    el.style.backgroundColor = currentColor;
+                  } else {
+                    el.style.color = currentColor;
+                  }
+                }, 1000);
+              }, delay);
+            } else {
+
+              setTimeout(() => {
+                if (binding.arg == 'background') {
+                  el.style.backgroundColor = binding.value;
+                } else {
+                  el.style.color = binding.value;
+                }
+              }, delay);
+            }
           }
         }
       }
