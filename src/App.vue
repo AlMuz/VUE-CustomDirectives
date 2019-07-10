192c1fa30ef1e3992142520cec3f846c1487af61
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-07-10 12:14:03 +0300

    added custom directive

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
+                <h1>Built-in Directives</h1>
+                <p v-text="'text from directive'"></p>
+                <p v-html="'<strong>text from directive</strong>'"></p>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight>Some text</p>
             </div>
         </div>
     </div>
